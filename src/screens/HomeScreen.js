afa16631273d481e1f00e988ab498a89b95044b8
nissimo1586 <131914709+nissimo1586@users.noreply.github.com>
2023-05-01 15:55:58 +0200

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,137 +1,134 @@
+/*import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
   Image,
-  StyleSheet,
   TouchableOpacity,
   ScrollView,
 } from 'react-native';
-
-// Importing event data and styles
-import events from '../events/events';
-import homeScreenStyles from './homeScreenStyles';
-
 import { connect } from 'react-redux';
 import { addEvent, removeEvent } from '../events/selectedEventsActions';
-
-// Importing Linking and images
-import { Linking } from 'react-native';
+import homeScreenStyles from './homeScreenStyles';
 import joinIcon from '../../assets/musix2.png';
 import eventIcon_no from '../../assets/heart_white_shape.png';
 import eventIcon_yes from '../../assets/heart_filled_red.png';
 import eventIcon_eventually from '../../assets/heart_white_shape_small.png';
-
-
-// Importing React and the useState hook
-import React, { useState } from 'react';
-
-// Importing useNavigation
 import { useNavigation } from '@react-navigation/native';
 
-// Defining the HomeScreen component
 const HomeScreen = (props) => {
-  // Using useNavigation to get access to the navigation prop
+  const [events, setEvents] = useState([]);
   const navigation = useNavigation();
 
-  // Defining a function to render a single event
+  useEffect(() => {
+    fetch('http://178.254.23.143:5000/api/events')
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   const renderEvent = (event) => {
-    // Using the useState hook to keep track of the current heart icon
     const [currentIcon, setCurrentIcon] = useState(eventIcon_no);
-
-    // Defining the categories for the My Events screen
-    const CATEGORIES = ['will take part', 'attendance maybe'];
-
-    // Using the useState hook to keep track of the event category
     const [eventCategory, setEventCategory] = useState(null);
 
-    // Toggling the icon status and category when the icon is clicked
     const toggleIconStatus = () => {
       let newIcon, newCategory;
       if (currentIcon === eventIcon_no) {
         newIcon = eventIcon_yes;
         newCategory = 'will take part';
-        props.addEvent({ ...event, category: newCategory }); // Dispatch the addEvent action
+        props.addEvent({ ...event, category: newCategory });
       } else if (currentIcon === eventIcon_yes) {
         newIcon = eventIcon_eventually;
         newCategory = 'attendance maybe';
-        props.addEvent({ ...event, category: newCategory }); // Dispatch the addEvent action
+        props.addEvent({ ...event, category: newCategory });
       } else {
         newIcon = eventIcon_no;
         newCategory = null;
-        props.removeEvent(event.id); // Dispatch the removeEvent action
+        props.removeEvent(event.id);
       }
       setCurrentIcon(newIcon);
       setEventCategory(newCategory);
     };
 
-    // Returning a TouchableOpacity that wraps around the event details
     return (
       <TouchableOpacity
         activeOpacity={1}
         onPress={() => {
           navigation.navigate('EventDetails', { event });
         }}>
-        {/* Defining the event container */}
         <View style={homeScreenStyles.eventContainer}>
           <Image source={joinIcon} style={homeScreenStyles.eventCategoryIcon} />
-
-          {/* Displaying the date and time */}
           <View style={homeScreenStyles.eventTitleTextContainer}>
             <Text style={homeScreenStyles.eventDateTimeText}>
               {event.date} | {event.time}
             </Text>
           </View>
-
-          {/* Displaying the event image */}
           <View style={homeScreenStyles.eventImageContainer}>
             <TouchableOpacity onPress={() => navigation.navigate('EventDetails', { event })}>
-              <Image
-                source={{ uri: event.image }}
-                style={homeScreenStyles.eventImage}
-              />
+              <Image source={{ uri: event.image }} style={homeScreenStyles.eventImage} />
             </TouchableOpacity>
             <View style={homeScreenStyles.overlay} />
           </View>
-
-          {/* Displaying the event title, date/time, and location */}
           <View style={homeScreenStyles.eventDetailsContainerbf}>
-            {/* Displaying the heart icon,"Maybe" button, and "Menu" button */}
-<View style={homeScreenStyles.eventFooterContainer}>
-<TouchableOpacity onPress={toggleIconStatus}>
-<Image
-               source={currentIcon}
-               style={homeScreenStyles.eventFooterButtonIcon}
-             />
-</TouchableOpacity>
-</View>
-<View style={homeScreenStyles.eventDetailsContainer}>
-<Text style={homeScreenStyles.eventTitleText}>{event.title}</Text>
-<Text style={homeScreenStyles.eventDateTimeText}>
-{event.date} | {event.time}
-</Text>
-<Text style={homeScreenStyles.eventLocationText}>
-{event.location}
-</Text>
-</View>
-</View>
-</View>
-</TouchableOpacity>
-);
-};
+            <View style={homeScreenStyles.eventFooterContainer}>
+              <TouchableOpacity onPress={toggleIconStatus}>
+                <Image source={currentIcon} style={homeScreenStyles.eventFooterButtonIcon} />
+              </TouchableOpacity>
+            </View>
+            <View style={homeScreenStyles.eventDetailsContainer}>
+              <Text style={homeScreenStyles.eventTitleText}>{event.title}</Text>
+              <Text style={homeScreenStyles.eventDateTimeText}>
+                {event.date} | {event.time}
+              </Text>
+              <Text style={homeScreenStyles.eventLocationText}>{event.location}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
-// Returning the HomeScreen component, which includes a ScrollView that
-// maps over the events and renders each one using the renderEvent function
-return (
-<View style={homeScreenStyles.container}>
-<ScrollView>
-{events.map((event) => {
-return renderEvent(event);
-})}
-</ScrollView>
-</View>
-);
-};
-
+  return (
+    <View style={homeScreenStyles.container}>
+      <ScrollView>
+        {events.map((event) => {
+          return renderEvent(event);
+        })}
+      </ScrollView>
+    </View>
+  );
+  };
+  
 // Wrap the HomeScreen with the connect() function
 const mapDispatchToProps = { addEvent, removeEvent };
-export default connect(null, mapDispatchToProps)(HomeScreen);
+export default connect(null, mapDispatchToProps)(HomeScreen);*/
+
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  ScrollView,
+} from 'react-native';
+import homeScreenStyles from './homeScreenStyles';
+import EventItem from './eventItem';
+
+const HomeScreen = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetch('http://178.254.23.143:5000/api/events')
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <View style={homeScreenStyles.container}>
+      <ScrollView>
+        {events.map((event) => {
+          return <EventItem key={event.id} event={event} />;
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default HomeScreen;
---
+++ src/screens/eventItem.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import homeScreenStyles from './homeScreenStyles';
+import joinIcon from '../../assets/musix2.png';
+import eventIcon_no from '../../assets/heart_white_shape.png';
+import eventIcon_yes from '../../assets/heart_filled_red.png';
+import eventIcon_eventually from '../../assets/heart_white_shape_small.png';
+import { useNavigation } from '@react-navigation/native';
+import { connect } from 'react-redux';
+import { addEvent, removeEvent } from '../events/selectedEventsActions';
+
+const EventItem = (props) => {
+  const { event } = props;
+  const [currentIcon, setCurrentIcon] = useState(eventIcon_no);
+  const [eventCategory, setEventCategory] = useState(null);
+  const navigation = useNavigation();
+
+  const toggleIconStatus = () => {
+    let newIcon, newCategory;
+    if (currentIcon === eventIcon_no) {
+      newIcon = eventIcon_yes;
+      newCategory = 'will take part';
+      props.addEvent({ ...event, category: newCategory });
+    } else if (currentIcon === eventIcon_yes) {
+      newIcon = eventIcon_eventually;
+      newCategory = 'attendance maybe';
+      props.addEvent({ ...event, category: newCategory });
+    } else {
+      newIcon = eventIcon_no;
+      newCategory = null;
+      props.removeEvent(event.id);
+    }
+    setCurrentIcon(newIcon);
+    setEventCategory(newCategory);
+  };
+
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      onPress={() => {
+        navigation.navigate('EventDetails', { event });
+      }}>
+      <View style={homeScreenStyles.eventContainer}>
+        <Image source={joinIcon} style={homeScreenStyles.eventCategoryIcon} />
+        <View style={homeScreenStyles.eventTitleTextContainer}>
+          <Text style={homeScreenStyles.eventDateTimeText}>
+            {event.date} | {event.time}
+          </Text>
+        </View>
+        <View style={homeScreenStyles.eventImageContainer}>
+          <TouchableOpacity onPress={() => navigation.navigate('EventDetails', { event })}>
+            <Image source={{ uri: event.image }} style={homeScreenStyles.eventImage} />
+          </TouchableOpacity>
+          <View style={homeScreenStyles.overlay} />
+        </View>
+        <View style={homeScreenStyles.eventDetailsContainerbf}>
+          <View style={homeScreenStyles.eventFooterContainer}>
+            <TouchableOpacity onPress={toggleIconStatus}>
+              <Image source={currentIcon} style={homeScreenStyles.eventFooterButtonIcon} />
+            </TouchableOpacity>
+          </View>
+          <View style={homeScreenStyles.eventDetailsContainer}>
+            <Text style={homeScreenStyles.eventTitleText}>{event.title}</Text>
+            <Text style={homeScreenStyles.eventDateTimeText}>
+              {event.date} | {event.time}
+            </Text>
+            <Text style={homeScreenStyles.eventLocationText}>{event.location}</Text>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const mapDispatchToProps = { addEvent, removeEvent };
+export default connect(null, mapDispatchToProps)(EventItem);
