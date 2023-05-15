bfd8b7c657cf7bd7d87200bc3bda8742b8c435bb
nissimo1586 <131914709+nissimo1586@users.noreply.github.com>
2023-05-15 17:06:31 +0200

    toda

---
+++ server/run_s.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://event_min_user:xFq2u56@2@localhost/event_min'  # MySQL connection URL
+db = SQLAlchemy(app)
+
+# Event model
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    date = db.Column(db.String(20))
+    time = db.Column(db.String(10))
+    image = db.Column(db.String(200))
+    location = db.Column(db.String(100))
+    description = db.Column(db.Text)  # New description column
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'date': self.date,
+            'time': self.time,
+            'image': self.image,
+            'location': self.location,
+            'description': self.description  # Include description in the dictionary
+        }
+
+@app.route('/api/events')
+def get_events():
+    # Retrieve events from the database
+    events = Event.query.all()
+
+    # Convert events to a list of dictionaries
+    event_list = [event.to_dict() for event in events]
+
+    # Return events as JSON response
+    return jsonify(event_list)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
