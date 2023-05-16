2c9ffec0abfea68404ce39e821496fc48c33e0e7
nissimo1586 <131914709+nissimo1586@users.noreply.github.com>
2023-05-16 10:06:24 +0200

    #1

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,32 +1,3 @@
-/*import React, { useState, useEffect } from "react";
-import { View, ScrollView } from "react-native";
-import homeScreenStyles from "./homeScreenStyles";
-import EventItem from "./eventItem";
-
-const HomeScreen = () => {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    fetch("http://178.254.23.143:5000/api/events")
-      .then((response) => response.json())
-      .then((data) => setEvents(data))
-      .catch((error) => console.error(error));
-  }, []);
-
-  return (
-    <View style={homeScreenStyles.container}>
-      <ScrollView>
-        {events.map((event) => {
- return <EventItem key={event.id} event={event} />;
-        })}
-      </ScrollView>
-    </View>
-  );
-};
-
-export default HomeScreen;
-*/
-
 import React, { useState, useEffect } from "react";
 import { View, ScrollView, Text, ActivityIndicator } from "react-native";
 import homeScreenStyles from "./homeScreenStyles";
